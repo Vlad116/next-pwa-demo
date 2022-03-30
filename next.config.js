f4f910d3123507dba4b700349c154ad37af6a104
Алексеев Владислав Алексеевич <VAAlekseev@ibs.root.ibscorp.ru>
2022-03-30 13:35:18 +0300

    init project :sparkles:

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withPWA = require("next-pwa");
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
